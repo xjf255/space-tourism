a4b31d79f4d35e909cf13ca8e6223dcdd1a396ba
xjf255 <juanfher.255@gmail.com>
2024-02-16 16:48:34 -0600

    add responsive mobile and tablet

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,12 +5,17 @@ import './nav.css'
 export function Nav () {
   const { position, setPosition } = useNav()
 
+  const exitMenu = () => {
+    document.querySelector('.nav--responsive > ul').classList.toggle('nav__ul--show')
+    document.querySelector('main').classList.toggle('section--bloqued')
+  }
+
   return (
     <nav className='nav__header'>
       <figure>
         <img src='assets/shared/logo.svg' alt='logo' />
       </figure>
-      <ul>
+      <ul className='nav--desktop'>
         {
           itemNav.map((title, index) => {
             return (
@@ -26,6 +31,30 @@ export function Nav () {
           })
         }
       </ul>
+      <section className='nav--responsive'>
+        <figure onClick={exitMenu}>
+          <ion-icon name='menu-outline' />
+        </figure>
+        <ul className='nav__ul--hide-responsive'>
+          <figure onClick={exitMenu}>
+            <ion-icon name='close-outline' />
+          </figure>
+          {
+            itemNav.map((title, index) => {
+              return (
+                <li
+                  key={title}
+                  className={title === position ? 'li--active' : null}
+                  onClick={() => { setPosition(title) }}
+                >
+                  <span className='nav__header--index'>0{index}</span>
+                  {title}
+                </li>
+              )
+            })
+          }
+        </ul>
+      </section>
     </nav>
   )
 }
